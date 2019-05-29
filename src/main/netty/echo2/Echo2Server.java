33cba5440f76d1b4f70e5a4f6edbefa68477fb36
daiyh <daiyh@midea.com>
2019-05-29 09:25:43 +0800

    dyh 指定长度的文本解码器FixedLengthFrameDecoder解决TCP粘包/拆包问题

---
+++ src/main/netty/echo2/Echo2Server.java
@@ -0,0 +1,78 @@
+package echo2;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.FixedLengthFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: daiyunhao
+ * Date: 19-5-28
+ * Description: FixedLengthFrameDecoder 以分隔符切换的文本解码器
+ */
+public class Echo2Server {
+    public void bind(int port) throws Exception{
+        //服务端NIO线程组
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workGroup = new NioEventLoopGroup();
+
+        ServerBootstrap b = new ServerBootstrap();
+        b.group(bossGroup, workGroup).channel(NioServerSocketChannel.class)
+                .option(ChannelOption.SO_BACKLOG, 100)
+                .handler(new LoggingHandler(LogLevel.INFO))
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel socketChannel) throws Exception {
+                        socketChannel.pipeline().addLast(new FixedLengthFrameDecoder(20));
+                        socketChannel.pipeline().addLast(new StringDecoder());
+                        socketChannel.pipeline().addLast(new Echo2ServerHandler());
+                    }
+                });
+
+        try {
+            ChannelFuture f = b.bind(port).sync();
+            f.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        int port = 8080;
+        if (args != null && args.length > 0){
+            port = Integer.valueOf(args[0]);
+        }
+
+        new Echo2Server().bind(port);
+    }
+}
+
+@ChannelHandler.Sharable
+class Echo2ServerHandler extends ChannelInboundHandlerAdapter{
+    private int counter = 0;
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+//        String body = (String) msg;
+//        System.out.println("This is " + ++counter + "times receive client: [" + body + "]");
+//        body += "$_";
+//        ByteBuf echo = Unpooled.copiedBuffer(body.getBytes());
+//        ctx.writeAndFlush(echo);
+        System.out.println("Receive Client: [ " + msg + " ]");
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
