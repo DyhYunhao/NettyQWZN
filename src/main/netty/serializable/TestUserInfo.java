14ecca50293400b2cc81f5f692130ab632a1c113
daiyh <daiyh@midea.com>
2019-05-29 10:19:40 +0800

    dyh java原生序列化的码流大小

---
+++ src/main/netty/serializable/TestUserInfo.java
@@ -0,0 +1,29 @@
+package serializable;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: daiyunhao
+ * Date: 19-5-29
+ * Description:
+ */
+public class TestUserInfo {
+
+    public static void main(String[] args) throws IOException {
+        UserInfo info = new UserInfo("Welcome to netty", 100);
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutputStream os = new ObjectOutputStream(bos);
+        os.writeObject(info);
+        os.flush();
+        os.close();
+        byte[] b = bos.toByteArray();
+        System.out.println("The jdk serializable length is: " + b.length);
+        bos.close();
+        System.out.println("---------------------------------------------");
+        System.out.println("the byteArray serializable length is: " + info.codeC().length);
+
+    }
+}
---
+++ src/main/netty/serializable/UserInfo.java
@@ -0,0 +1,53 @@
+package serializable;
+
+import org.omg.CORBA.PUBLIC_MEMBER;
+
+import java.io.Serializable;
+import java.nio.ByteBuffer;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: daiyunhao
+ * Date: 19-5-29
+ * Description:
+ */
+public class UserInfo implements Serializable {
+    private static final long serialVersionUid = 1L;
+
+    private String username;
+    private int userId;
+
+    public UserInfo(String username, int userId) {
+        this.username = username;
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public byte[] codeC(){
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+        byte[] value = this.username.getBytes();
+        byteBuffer.putInt(value.length);
+        byteBuffer.put(value);
+        byteBuffer.putInt(this.userId);
+        byteBuffer.flip();
+        value = null;
+        byte[] result = new byte[byteBuffer.remaining()];
+        byteBuffer.get(result);
+        return result;
+    }
+}
