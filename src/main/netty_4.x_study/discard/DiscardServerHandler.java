8e7cde3d8f42571efc7c1f1c12f7b801863f1279
daiyh <daiyh@midea.com>
2019-06-10 11:15:37 +0800

    dyh netty4.x的丢弃服务器

---
+++ src/main/netty_4.x_study/discard/DiscardServerHandler.java
@@ -0,0 +1,25 @@
+package discard;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: daiyunhao
+ * Date: 19-6-6
+ * Description:
+ */
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        //丢弃接收到的数据
+        ((ByteBuf) msg).release();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
